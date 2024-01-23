b4de5b9323f3413cf93d35c162aa913815c75f83
Manish Singh <contactmanishsingh7@gmail.com>
2024-01-24 01:29:44 +0530

    1st commit to new branch

--- src/com/practice/array/DeclareArray.java
+++ src/com/practice/array/DeclareArray.java
@@ -5,7 +5,7 @@ public class DeclareArray {
 	public static void main(String[] args) {
 		int a[] = { 1, 2, 3, 4, 5 };
 		for (int element : a)
-			System.out.println("Array of elements are :" + element);
+			System.out.println("Array of elements are : " + element);
 
 	}
 }
