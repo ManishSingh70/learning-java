46823b3f17a1adf9e9d48f9da558dd8df520a6b7
Manish Singh <contactmanishsingh7@gmail.com>
2024-01-23 23:09:03 +0530

    Eclipse First Commit

---
+++ src/com/google/array/DeclareArray.java
@@ -0,0 +1,11 @@
+package com.google.array;
+
+public class DeclareArray {
+
+	public static void main(String[] args) {
+		int a[] = { 1, 2, 3, 4, 5 };
+		for (int element : a)
+			System.out.println("Array of elements are :" + element);
+
+	}
+}
